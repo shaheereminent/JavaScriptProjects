e58fd038a46ac0ebf64048cdd284f48e6337cfc0
shaheereminent <shaheer_eminent@hotmail.com>
2024-12-15 04:41:26 +0500

    challenge complete

--- 01-Fundamentals-Part-1/challenges/TipCalculator/TipCalculator.js
+++ 01-Fundamentals-Part-1/challenges/TipCalculator/TipCalculator.js
@@ -1,6 +1,6 @@
-let bill = 275
+let bill = 430
 let tip
 
-bill > 50 && bill < 300 ? (tip = (bill * 15) / 100) : (bill * 20) / 100
+bill > 50 && bill < 300 ? (tip = (bill * 15) / 100) : (tip = (bill * 20) / 100)
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value
-316.25`)
+is ${tip + bill}`)
